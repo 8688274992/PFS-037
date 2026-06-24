f3492776d685fc46293b23d7a994984fc4c19d1b
Prameela Kandi <kandiprameela96@gmail.com>
2026-06-24 19:34:45 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,145 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+print(3+31)
+34
+a=8
+print(a)
+8
+b=2
+c=7
+print(b,c)
+2 7
+b=2
+v=7
+print(b+c)
+9
+a=20
+print(A)
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    print(A)
+NameError: name 'A' is not defined. Did you mean: 'a'?
+a2=34
+print(a2)
+34
+3q=45
+SyntaxError: invalid decimal literal
+a1234567=0
+print(a1234567)
+0
+3=89
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+print("ammu")
+ammu
+name="ammu"
+print("name")
+name
+city="vijayawada"
+print("city")
+city
+@a=90
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+_=67
+print(_)
+67
+_value=56
+print(_value)
+56
+mailid="prameelakandi@96gmail.com")
+SyntaxError: unmatched ')'
+print("mailid")
+mailid
+mailid
+Traceback (most recent call last):
+  File "<pyshell#29>", line 1, in <module>
+    mailid
+NameError: name 'mailid' is not defined
+mailid="prameelakandi@96gmail.com"
+mailid="prameelakandi@96gmail.com"
+print("mailid")
+mailid
+
+mailid="prameelakandi@96gmail.com"
+print("mailid")
+mailid
+mailid
+'prameelakandi@96gmail.com'
+KeyboardInterrupt
+mailid="prameelakandi@96gmail.com"
+print(mailid)
+prameelakandi@96gmail.com
+a=4;b=6
+print(a+b)
+10
+a,b=3,4
+print(a,b)
+3 4
+'
+a,b,c=10
+Traceback (most recent call last):
+  File "<pyshell#43>", line 1, in <module>
+    a,b,c=10
+TypeError: cannot unpack non-iterable int object
+a=b=c=34
+print(a,b,c)
+34 34 34
+if=2
+SyntaxError: invalid syntax
+elif=34
+SyntaxError: invalid syntax
+print(print)
+<built-in function print>
+a,b,s=1,2,3
+print(a,b,s)
+1 2 3
+a,s,d=1,2,3,4,5,6
+Traceback (most recent call last):
+  File "<pyshell#51>", line 1, in <module>
+    a,s,d=1,2,3,4,5,6
+ValueError: too many values to unpack (expected 3, got 6)
+>>> a,s,d=(2,3,4)
+>>> print(a,s,d)
+2 3 4
+>>> first name="ammu"
+SyntaxError: invalid syntax
+>>> first_name="ammu"
+>>> print(first_name)
+ammu
+>>> firstname="ammu"
+>>> print(firstname)
+ammu
+>>> fname="ammu"
+>>> print(fname)
+ammu
+>>> fname="ammu"
+>>> lname="prameela"
+>>> print(fname+lname)
+ammuprameela
+>>> print(fname+" "lname)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> print(fname+" "lname)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> print(fname+" "+lname)
+ammu prameela
+>>> print(fname,lname)
+ammu prameela
+>>> name="ammu"
+>>> print(name)'
+SyntaxError: unterminated string literal (detected at line 1)
+>>> print(name)
+ammu
+>>> NAME="ammu"
+>>> print(NAME)
+ammu
+>>> Name="ammu"
+>>> print(Name)
+ammu
+>>> a=9
+>>> print(a)
+9
+>>> del a
+>>> print(a)
+Traceback (most recent call last):
+  File "<pyshell#78>", line 1, in <module>
+    print(a)
+NameError: name 'a' is not defined. Did you mean: 'a2'?
