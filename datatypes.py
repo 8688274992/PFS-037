2fa1f5378065c529dfbb79c9f943ee36444dbf4e
Prameela Kandi <kandiprameela96@gmail.com>
2026-06-26 22:16:05 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,27 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #datatypes
+>>> a=0
+>>> type(a)
+<class 'int'>
+>>> b=9.0
+>>> type(b)
+<class 'float'>
+>>> c='code'
+>>> type(c)
+<class 'str'>
+>>> c="code"
+>>> type(c)
+<class 'str'>
+>>> c='''ammu'''
+>>> type(c)
+<class 'str'>
+>>> d=2+2j
+>>> type(d)
+<class 'complex'>
+>>> p=True
+>>> type(p)
+<class 'bool'>
+>>> j=False
+>>> type(j)
+<class 'bool'>
