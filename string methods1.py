d974ef3625142cd60ccbcdb9430cd6b6892e344f
Prameela Kandi <kandiprameela96@gmail.com>
2026-07-01 19:09:47 +0530

    Add files via upload

---
+++ string methods1.py
@@ -0,0 +1,43 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #string methods
+>>> #length
+>>> a="python"
+>>> len(a)
+6
+>>> b="python course"
+>>> len(b)
+13
+>>> c=""
+>>> len(c)
+0
+>>> c=" "
+>>> len(c)
+1
+>>> #count
+>>> a="twinkle twinkle little star"
+>>> a.count("twinkle")
+2
+>>> a.count("l")
+4
+>>> a.count(" ")
+3
+>>> a.count(w")
+...         
+SyntaxError: unterminated string literal (detected at line 1)
+>>> a.count("w")
+...         
+2
+>>> #find a string
+...         
+>>> a="code"
+...         
+>>> s="hello"
+...         
+>>> 
+>>> 
+>>> s="hello"
+...         
+>>> b.find("l")
+...         
+-1
